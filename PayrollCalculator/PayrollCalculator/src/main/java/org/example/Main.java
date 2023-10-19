13f0b95fe45157b3ad25fbb1955e000dd570b4e5
SleamanAbdelhadi <sabdelhadi@appdev.yearup.org>
2023-10-19 15:17:25 -0400

    added PayrollCalculator

--- PayrollCalculator/PayrollCalculator/src/main/java/org/example/Main.java
+++ PayrollCalculator/PayrollCalculator/src/main/java/org/example/Main.java
@@ -1,28 +1,52 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
 
 
-    public static void main(String[] args) {
-        try  {
-            BufferedReader  bufferRead = new BufferedReader (new FileReader ("employees.csv")) ;
-            String ReaderLine ;
-            bufferRead.readLine();
-            while ((ReaderLine = bufferRead.readLine()) != null) {
-                String[] data = ReaderLine.split( "\\|") ;
-                var employeeName=data[1];
-                var employeeId=Integer.parseInt(data[0]);
-                var hoursWorked= Double.parseDouble(data[2]);
-                var payRate= Double.parseDouble(data[3]);
-                Employee employee = new Employee(employeeId,employeeName,hoursWorked,payRate);
-                System.out.printf("\nEmployeeID: %d, \nEmployeeName: %s, \nEmployeePay: $%.2f \n",employee.getEmployeeID(),employee.getName(),employee.getCrossPay());
-            }
-            bufferRead.close();
+          public static void main(String[] args) {
+              try  {
+           boolean done=false ;
+           while (!done) {
+               Scanner scanner = new Scanner(System.in);
+               System.out.println("enter the name of employee file to process");
+               String fileName = scanner.nextLine();
+
+               if (fileName.equals("employees.csv")) {
+                   System.out.println("enter the name of payroll file");
+                   String payrollFileName = scanner.nextLine();
+
+                   FileWriter fileWriter = new FileWriter(payrollFileName);
+                   BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+                   BufferedReader bufferRead = new BufferedReader(new FileReader("employees.csv"));
+                   String ReaderLine;
+                   bufferRead.readLine();
+                   while ((ReaderLine = bufferRead.readLine()) != null) {
+
+                       String[] data = ReaderLine.split("\\|");
+                       var employeeName = data[1];
+                       var employeeId = Integer.parseInt(data[0]);
+                       var hoursWorked = Double.parseDouble(data[2]);
+                       var payRate = Double.parseDouble(data[3]);
+                       Employee employee = new Employee(employeeId, employeeName, hoursWorked, payRate);
+                       System.out.printf("\nEmployeeID: %d, \nEmployeeName: %s, \nEmployeePay: $%.2f \n", employee.getEmployeeID(), employee.getName(), employee.getCrossPay());
+
+                       String output;
+                       output = String.format("%d|%s|%.2f\n",employee.getEmployeeID(),employee.getName(),employee.getCrossPay()) ;
+                    bufWriter.write(output);
+                   }
+                   bufferRead.close();
+                   bufWriter.close();
+                   done=true;
+               }
+               else{
+                   System.out.println("enter correct employee file name");
+               }
+           }
         } catch (IOException e)  {
          e.printStackTrace();
         }
